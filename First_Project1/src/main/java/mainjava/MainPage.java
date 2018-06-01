845d43c6d607afaaf4241de3de72fb00a4e03898
prakashbhande <prakashbhande@gmail.com>
2018-06-01 06:22:45 +0530

    first git code

---
+++ First_Project1/src/main/java/mainjava/MainPage.java
@@ -0,0 +1,13 @@
+package mainjava;
+
+import org.testng.annotations.Test;
+
+public class MainPage {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Hello Java");
+	}
+
+}
---
+++ First_Project1/src/test/java/testjava/TestPage.java
@@ -0,0 +1,12 @@
+package testjava;
+
+import org.testng.annotations.Test;
+
+public class TestPage {
+	@Test
+	public void test1()
+	{
+		System.out.println("Hello Test Case");
+	}
+
+}
